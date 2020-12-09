07897622ff2ec0c5113868f2266aeefde2dc9bf9
PS <ps@PSs-MacBook-Pro.local>
2020-12-09 11:45:32 +0530

    Refactored Inference.py

--- scripts/inference.py
+++ scripts/inference.py
@@ -41,7 +41,7 @@ def get_parser():
 
     parser.add_argument(
         "--save-dir",
-        default=None,
+        default='./',
         type=str,
         help="location to save prediction file"
     )
@@ -51,12 +51,12 @@ def get_parser():
 def load_model(model_path):
     return joblib.load(model_path)
 
-def get_embed(file):
-    return np.asarray(VoiceEncoder().embed_utterance(preprocess_wav(file))).reshape(1, -1)
+def get_embed(voice_enc, file):
+    return np.asarray(voice_enc.embed_utterance(preprocess_wav(file))).reshape(1, -1)
 
-def get_prediction(model, file):
+def get_prediction(voice_enc,model, file):
     if os.path.exists(file):
-        X = get_embed(file)
+        X = get_embed(voice_enc,file)
         return model.predict(X)[0]
     else:
         raise Exception(f"File path does not exist {file}")
@@ -64,14 +64,13 @@ def get_prediction(model, file):
 def main(args):
     if os.path.exists(args.model_path):
         model = load_model(args.model_path)
-
+    voice_enc = VoiceEncoder()
     if args.file_mode:
-        print(f"Predicted gender : {get_prediction(model, args.file_path)}")
+        print(f"Predicted gender : {get_prediction(voice_enc,model, args.file_path)}")
     else:
         df = pd.read_csv(args.csv_path, header=None, names=['file_paths'])
-        df['predicted_gender'] = Parallel(n_jobs=-1)(delayed(get_prediction)(model, i) for i in tqdm(df['file_paths'].values))
+        df['predicted_gender'] = Parallel(n_jobs=-1)(delayed(get_prediction)(voice_enc,model, file_path) for file_path in tqdm(df['file_paths'].values))
         df.to_csv(os.path.join(args.save_dir, 'predictions.csv'), header=False, index=False)
-
         print(f"Inference Completed")
 
 if __name__ == "__main__":
