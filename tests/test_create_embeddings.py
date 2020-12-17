fc95bbef6bda5e45ce9546919f38e98ef16a5c92
Neeraj Chhimwal <neerajchhimwal@Neerajs-MacBook-Pro.local>
2020-12-17 16:45:26 +0530

    Added more test cases for inference and embed creation

--- tests/test_create_embeddings.py
+++ tests/test_create_embeddings.py
@@ -1,9 +1,55 @@
 import unittest
+from scripts.create_embeddings import encoder
+from os import path
+from glob import glob
+import numpy as np
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+class EmbeddingsTests(unittest.TestCase):
+    def test_should_create_embeddings_for_clean_test_files(self):
+        source_dir = '../resources/test_data/'
+        source_dir_pattern = '*/*/*/clean/*.wav'
+        embed_file_name = '../resources/test_outputs/sample_embed.npz'
+
+        encoder(source_dir, source_dir_pattern, embed_file_name)
+        self.assertTrue(path.exists(embed_file_name))
+        clean_files = glob(source_dir + source_dir_pattern)
+        self.assertEqual(len(clean_files), 16)
+
+    def test_embed_file_created_has_4_columns(self):
+        embed_file_name = '../resources/test_outputs/sample_embed.npz'
+        embed_file = np.load(embed_file_name)
+        self.assertEqual(embed_file.files, ['embeds', 'file_paths', 'gender', 'duration'])
+
+    def test_should_extract_gender_from_dir_and_save_in_embed_file(self):
+        embed_file_name = '../resources/test_outputs/sample_embed.npz'
+        embed_file = np.load(embed_file_name)
+        gender_list = embed_file['gender']
+        self.assertTrue('male' in gender_list and 'female' in gender_list)
+        num_male_samples = [i for i in gender_list if i == 'male']
+        num_female_samples = [i for i in gender_list if i == 'female']
+
+        self.assertEqual(len(num_male_samples), 8)
+        self.assertEqual(len(num_female_samples), 8)
+
+    def test_should_extract_audio_duration_and_save_in_embed_file(self):
+        embed_file_name = '../resources/test_outputs/sample_embed.npz'
+        embed_file = np.load(embed_file_name)
+        durations = embed_file['duration']
+        dtypes = [str(type(dur)) for dur in durations]
+        self.assertTrue(all([True for dtype in dtypes if 'float64' in dtype]))
+
+    def test_should_extract_file_paths_and_save_in_embed_file(self):
+        embed_file_name = '../resources/test_outputs/sample_embed.npz'
+        embed_file = np.load(embed_file_name)
+        file_paths = embed_file['file_paths']
+        self.assertTrue(all([path.exists(file) for file in file_paths]))
+
+    def test_embeddings_have_256_dimensions(self):
+        embed_file_name = '../resources/test_outputs/sample_embed.npz'
+        embed_file = np.load(embed_file_name)
+        embeds = embed_file['embeds']
+        self.assertEqual(embeds.shape, (16, 256))
 
 
 if __name__ == '__main__':
--- tests/test_inference.py
+++ tests/test_inference.py
@@ -1,9 +1,37 @@
 import unittest
+from scripts.inference import get_prediction, get_prediction_csv_mode, load_model, get_prediction_from_npz_file
+from resemblyzer import VoiceEncoder, preprocess_wav
+from os import path
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+class InferenceTests(unittest.TestCase):
+    def test_returns_int_value_for_gender_in_file_mode(self):
+        file_path = '../resources/test_data/bucket_sample/female/202009141109586182/clean/1_24_378file-idqUQWlr1113g.wav'
+        model_path = '../model/clf_svc.sav'
+        model = load_model(model_path)
+        voice_enc = VoiceEncoder()
+        prediction = get_prediction(voice_enc, model, file_path)
+        self.assertEqual(prediction, 1)
+
+    def test_returns_csv_with_predicted_genders_in_csv_mode(self):
+        csv_path = '../resources/sample_csv_mode_data.csv'
+        model_path = '../model/clf_svc.sav'
+        save_dir = '../resources/test_outputs/'
+        model = load_model(model_path)
+        voice_enc = VoiceEncoder()
+        get_prediction_csv_mode(voice_enc, model, csv_path, save_dir)
+        self.assertTrue(path.exists(save_dir + 'predictions.csv'))
+
+    def test_returns_csv_with_predicted_genders_from_npz_file(self):
+        npz_file_path = '../resources/tarini.npz'
+        model_path = '../model/clf_svc.sav'
+        save_dir = '../resources/test_outputs/'
+        model = load_model(model_path)
+        file_vs_prediction_df = get_prediction_from_npz_file(model, npz_file_path, save_dir)
+        # self.assertTrue(path.exists(save_dir+'predictions_from_npz.csv'))
+        gender_values = set(list(file_vs_prediction_df['gender']))
+        self.assertEqual(list(file_vs_prediction_df.columns), ['file_paths', 'gender'])
+        self.assertEqual({0, 1}, gender_values)
 
 
 if __name__ == '__main__':
